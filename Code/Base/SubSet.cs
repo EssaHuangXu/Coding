0393158852e7885306eec3b326c2cf9da979d2a7
Essa <hxEssa@gmail.com>
2024-12-31 20:41:48 +0800

    完全子集

---
+++ Code/Base/SubSet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Base
+{
+	public class SubSet
+	{
+		public static IList<IList<int>> Subsets( int[] nums )
+		{
+			var result = new List<IList<int>>();
+			var count = nums.Length;
+			for ( int i = 0; i < (int)( 1 << count ); i ++ )
+			{
+				List<int> list = new List<int>();
+				for( int j = 0; j < count; j++ )
+				{
+					if( ( i >> j & 1 ) == 1 )
+					{
+						list.Add( nums[j] );
+					}
+				}
+				result.Add( list );
+			}
+			return result;
+		}
+	}
+}
